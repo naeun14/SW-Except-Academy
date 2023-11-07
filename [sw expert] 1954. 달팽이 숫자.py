f9395d4a877084933b03651bd3b26ad123dad8a5
naeun14 <kmiyeon123@gmail.com>
2023-11-07 17:33:06 +0900

    1954 문제 해결

--- [sw expert] 1954. 달팽이 숫자.py
+++ [sw expert] 1954. 달팽이 숫자.py
@@ -1,6 +1,25 @@
-
-def make_round():
-    for i in range()
 T = int(input())
 for test_case in range(1, T + 1):
+    N = int(input())
+    snail = [[0]*N for _ in range(N)] #0으로 초기화
+    count = 1  #배열 입력값 
+    dir = 0 #0:우 1:하 2:좌 3:상 
+    dr = [0,1,0,-1]
+    dc = [1,0,-1,0]
+    row,col = 0,0
+    while count <= N*N: #n*n 배열 모두 채우기 
+        if (0 <= row < N and 0 <= col < N) and snail[row][col] == 0: #정상 범위 내라면 
+            snail[row][col] = count
+            count += 1
+        else:
+            row -= dr[dir]
+            col -= dc[dir]
+            dir = (dir+1)%4
+            
+        row += dr[dir]
+        col += dc[dir]
+        
+    print("#{}".format(T))
+    for r in snail:
+        print(*r)  #*를 붙이면 [] 떼고 출력 가능
     
